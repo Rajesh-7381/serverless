8c9fab4c7634e845cb955b6b29b9196a97b306ed
rajesh <aja@gmail.com>
2025-09-19 18:43:02 +0530

    first changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-dotenv.config()  ;
+dotenv.config() ;
 
 app.listen(process.env.PORT,()=>{
     console.log(`server listened at port ${process.env.PORT}`);
