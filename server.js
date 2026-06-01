81385abbec02e304fbe7c5c4555b313d3757611a
rajesh <aja@gmail.com>
2026-06-01 23:28:26 +0530

    added new logs

--- server.js
+++ server.js
@@ -16,3 +16,4 @@ app.listen(process.env.PORT, () => {
   console.log(`server listened at port ${process.env.PORT}`); 
 });
 console.log("🚀 Server is starting...");
+console.log("🚀 Server is starting...");
